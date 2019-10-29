0c371194011a22ef4a49894937b850d191b04099
liuyuan <hi60@qq.com>
2019-10-29 16:34:46 +0800

    init project

---
+++ src/structuralPattern/decorator/ComponentA.java
@@ -0,0 +1,17 @@
+package structuralPattern.decorator;
+
+/**
+ * @author liuyuan
+ * @create 2019-10-29 16:25
+ * @description
+ **/
+public class ComponentA implements Component {
+    public ComponentA() {
+        System.out.println("创建具体构件角色");
+    }
+
+    @Override
+    public void operation() {
+        System.out.println("调用具体构件角色的方法operation()");
+    }
+}
