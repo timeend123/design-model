7d7c8548bffa3decc94f6435fa8ef629e87024bc
timeend123 <hi60@qq.com>
2019-11-11 22:51:27 +0800

    add:test stackOverflow

---
+++ src/jvm/JavaVMStackSOF.java
@@ -0,0 +1,23 @@
+package jvm;
+
+/**
+ * @author liuyuan
+ * @create 2019-11-11 22:13
+ * @description
+ **/
+public class JavaVMStackSOF {
+    private int stackLength=1;
+    public void stackLeak(){
+        stackLength++;
+        stackLeak();
+    }
+    public static void main(String[] args) {
+        JavaVMStackSOF omm=new JavaVMStackSOF();
+        try {
+            omm.stackLeak();
+        }catch (Throwable e){
+            System.out.println("stackLength:"+omm.stackLength);
+            throw e;
+        }
+    }
+}
