938a7750cb1f04309306c114ea274157a8837c6b
liuyuan <hi60@qq.com>
2019-12-02 17:06:37 +0800

    update:Test

--- src/leetCodePractices/concurrency/Test.java
+++ src/leetCodePractices/concurrency/Test.java
@@ -9,43 +9,29 @@ import java.util.function.IntConsumer;
  **/
 public class Test {
     ZeroEvenOdd zeroEvenOdd=new ZeroEvenOdd(2);
-    IntConsumer printNumber= new IntConsumer() {
-        @Override
-        public void accept(int value) {
-            System.out.println(value);
-        }
-    };
+    IntConsumer printNumber= value -> System.out.println(value);
     //输出0
-    Thread thread1= new Thread(new Runnable() {
-        @Override
-        public void run() {
-            try {
-                zeroEvenOdd.zero(printNumber);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+    Thread thread1= new Thread(() -> {
+        try {
+            zeroEvenOdd.zero(printNumber);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     });
     //输出偶数
-    Thread thread2= new Thread(new Runnable() {
-        @Override
-        public void run() {
-            try {
-                zeroEvenOdd.even(printNumber);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+    Thread thread2= new Thread(() -> {
+        try {
+            zeroEvenOdd.even(printNumber);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     });
     //输出奇数
-    Thread Thread3= new Thread(new Runnable() {
-        @Override
-        public void run() {
-            try {
-                zeroEvenOdd.odd(printNumber);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+    Thread Thread3= new Thread(() -> {
+        try {
+            zeroEvenOdd.odd(printNumber);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     });
 
