5070ae7e967312323e65cefec2503e1503b143a7
liuyuan <hi60@qq.com>
2020-01-08 15:05:39 +0800

    update:两数相加

--- src/leetCodePractices/algorithms/AddTwoNumbers.java
+++ src/leetCodePractices/algorithms/AddTwoNumbers.java
@@ -17,37 +17,46 @@ public class AddTwoNumbers {
 
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        //初始化一个结果链表
         ListNode resultNode = new ListNode(0);
+        ListNode currentNode = resultNode, p = l1, q = l2, temp;
 
-        while (l1.next != null || l2.next != null) {
+        while (p != null || q != null) {
             //当前节点相加
-            resultNode.val = resultNode.val + l1.val + l2.val;
-            if (l1.next == null && l2.next != null) {
-                l1.next = new ListNode(0);
+            currentNode.val = currentNode.val + p.val + q.val;
+            if (p.next == null && q.next != null) {
+                p.next = new ListNode(0);
             }
-            if (l2.next == null && l1.next != null) {
-                l2.next = new ListNode(0);
+            if (q.next == null && p.next != null) {
+                q.next = new ListNode(0);
             }
-            resultNode.val = resultNode.val % 10;
-            resultNode.next = new ListNode(resultNode.val / 10);
-//            resultNode = resultNode.next;
-            l1 = l1.next;
-            l2 = l2.next;
+            temp = new ListNode(currentNode.val / 10);
+            currentNode.val = currentNode.val % 10;
+            currentNode.next = temp;
+            if (p.next == null && q.next == null&&currentNode.next.val==0) {
+                currentNode.next = null;
+            }
+            currentNode = temp;
+            p = p.next;
+            q = q.next;
         }
         return resultNode;
-
     }
 
     public static void main(String[] args) {
+
+
         ListNode result;
-        ListNode l1 = new ListNode(2);
+        ListNode l1 = new ListNode(5);
         l1.next = new ListNode(4);
         l1.next.next = new ListNode(3);
         ListNode l2 = new ListNode(5);
         l2.next = new ListNode(6);
         l2.next.next = new ListNode(4);
         result = new AddTwoNumbers().addTwoNumbers(l1, l2);
+        while (result != null) {
+            System.out.println(result.val);
+            result = result.next;
+        }
         System.out.println("结束");
     }
 }
